9a4781b3b7e2dcdc520cb5da61cb414e41dfaae4
Previo1 <rudakiyapiyush1@gmail.com>
2025-08-02 09:26:56 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBwaAAfND1dBnr_un2qA3YAy0Ipcpsl7JM",
   authDomain: "newquiz-4454f.firebaseapp.com",
@@ -10,6 +8,3 @@ const firebaseConfig = {
   appId: "1:268848107378:web:111595fbc275823ec5d7eb",
   measurementId: "G-RYH64757WM"
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
